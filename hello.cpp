c9d73b04add7bc3282eedc7ba1bf8dd5d50fd423
Taijon-Willis <twill227@ut.utm.edu>
2026-01-21 17:16:34 +0000

    Initial verison - incorrect output

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 /**
  * @file hello.cpp
- * @author your name (you@domain.com)
+ * @author Taijon Willis
  * @brief 
  */
 #include <iostream>
@@ -9,5 +9,5 @@ using namespace std;
 
 int main()
 {
-    // YOUR CODE HERE
+   cout << "Hello, world!" << endl; 
 }
